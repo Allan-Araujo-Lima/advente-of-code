6130af7b55c2e118f73180f5fee6e8f632d02c47
Allan Araújo lima <allanvoide@gmail.com>
2024-12-12 23:11:59 -0300

    solving day 3

---
+++ day2/day2.1.js
@@ -0,0 +1,56 @@
+const fs = require('fs').promises;
+
+fs.readFile('input2.txt', 'utf-8')
+    .then((text) => {
+        function processData(a, b, c, d, e) {
+            if (d == 2) return false;
+
+            if (Array.isArray(a) && a.length > 1) {
+                if (c === true) {
+                    if (a[b + 1] > a[b] && a[b] - a[b + 1] >= -3) {
+                        deletes.push(a.shift())
+                        console.log(deletes)
+                        return processData(a, 0, true, d, deletes)
+                    }
+                    a.shift()
+                    d++
+                    if (d == 2) return false;
+                    return processData(deletes.concat(a), 0, true, d, deletes)
+                } else if (c === false) {
+                    if (a[b + 1] < a[b] && a[b] - a[b + 1] <= 3) {
+                        deletes.push(a.shift())
+                        console.log(deletes)
+                        return processData(a, 0, false, d, deletes)
+                    }
+                    a.shift()
+                    d++
+                    if (d == 2) return false;
+                    return processData(deletes.concat(a), 0, true, d)
+                }
+            }
+            return true;
+        }
+
+        let allInputs = text.replace(/\s/g, ";");
+        allInputs = allInputs.replace(/;;/g, ",");
+        allInputs = allInputs.split(",")
+
+        let newAllInputs = []
+
+        for (var i = 0; i < allInputs.length; i++) {
+            newAllInputs.push(allInputs[i].split(";").map(Number))
+        }
+
+        let safe = 0;
+        let erro = 0;
+
+        for (var i = 0; i < newAllInputs.length; i++) {
+            if (newAllInputs[i][0] < newAllInputs[i][1]) {
+                safe += processData(newAllInputs[i], 0, true) ? 1 : 0
+            } else if (newAllInputs[i][0] > newAllInputs[i][1]) {
+                safe += processData(newAllInputs[i], 0, false) ? 1 : 0
+            }
+        }
+
+        console.log(safe)
+    })
--- day3/day3.js
+++ day3/day3.js
@@ -1,10 +1,40 @@
 const fs = require('fs').promises;
 
+const regex = /mul[(](\w+,\w+[)])/g;
+
 fs.readFile('input.txt', 'utf-8')
     .then((text) => {
-        let b = text.split('mul')
-        for (var i = 0; i < b.length; i++) {
-            b[i].split(')')
+        let input = []
+        let newInput = []
+        let start = 0;
+        let lastAppear = 'do()'
+        input = (text.match(regex));
+
+        for (var i = 0; i < input.length; i++) {
+            let prov = String(input[i]).substring(4, input[i].length - 1)
+            let end = text.indexOf(input[i])
+
+            let doo = text.substring(start, end).lastIndexOf("do()")
+            let dont = text.substring(start, end).lastIndexOf("don't()")
+
+            if (doo > dont) {
+                lastAppear = "do()"
+                newInput.push(prov.split(',').map(Number))
+            } else if (doo < dont) {
+                lastAppear = "don't()"
+            } else {
+                lastAppear === "do()" ? newInput.push(prov.split(',').map(Number)) : null
+            }
+
+            console.log(prov, lastAppear)
+            start = end
         }
-        console.log(b)
+
+        let result = 0;
+
+        for (var i = 0; i < newInput.length; i++) {
+            result += newInput[i][0] * newInput[i][1]
+        }
+
+        console.log(result)
     })
